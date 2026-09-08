ffd86e4cc65a8977f3f3ad2bf3f5cf42387c7107
TUNG5T3N <tung10802@gmail.com>
2026-09-08 09:53:38 +0700

    Lecture in class

--- Assets/Scripts/Workspace/Assignment/Lecture.cs
+++ Assets/Scripts/Workspace/Assignment/Lecture.cs
@@ -1,5 +1,6 @@
-using UnityEngine;
+using NUnit;
 using System.Collections.Generic;
+using UnityEngine;
 
 namespace Assignment
 {
@@ -32,7 +33,55 @@ namespace Assignment
 
         public void LCT04_SyntaxDictionary()
         {
-            throw new System.NotImplementedException();
+            Dictionary<string, int> dic = new Dictionary<string, int>();
+            var inv2 = new Dictionary<string, int>();
+
+            // "Potion": 1
+            inv.Add("Potion", 1);
+
+            // "Potion": 1
+            // "Apple": 10
+            inv.Add("Apple", 10);
+
+            // "Potion": 1
+            // "Apple": 10
+            // "Banana": 5
+            inv["Banana"] = 5;
+
+            // "Potion": 10
+            // "Apple": 10
+            // "Banana": 5
+            inv["Potion"] = 10;
+
+            var pickupItem = "Sword";
+            // "Potion": 10
+            // "Apple": 10
+            // "Banana": 5
+            // "Sword": 5
+            inv[pickupItem] = 1;
+
+            //foreach (KeyValuePair<string, int> pair in inv)
+            foreach ( var pair in inv)
+            {
+                string key = pair.Key;
+                int value = pair.Value;
+                Debug.Log($"Key: {key} value: {value}");
+            }
+
+            var appleExists = inv2.ContainsKey("Apple");
+            Debug.Log(appleExists);
+
+            var keyExists = inv2.ContainsKey("Key");
+            Debug.Log(keyExists);
+
+            inv2.Remove("Apple");
+
+            foreach (var pair in inv)
+            {
+                string key = pair.Key;
+                int value = pair.Value;
+                Debug.Log($"Key: {key} value: {value}");
+            }
         }
 
         #endregion
